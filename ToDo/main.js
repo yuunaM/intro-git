25def63bdf1152e2107e8f45404cbd522a507419
yuunaM <the.megacity@icloud.com>
2024-02-08 17:02:02 +0900

    .gitignore無視

---
+++ ToDo/main.js
@@ -0,0 +1,36 @@
+const addArea = document.getElementById('add-area');
+const addBtn = document.querySelector('.add-btn');
+const list = document.getElementById('todo');
+let addTxtValue;
+
+addBtn.addEventListener('click', () => {
+    addTxtValue = addArea.value;
+
+    if (addTxtValue.length === 0) { // 空欄なら処理終了
+        alert('空欄です！');
+        return;
+    }
+
+    // リスト作成
+    const li = document.createElement('li'); 
+    li.textContent = addTxtValue;
+
+    // 完了ボタン作成
+    const trashBtn = document.createElement('button');
+    trashBtn.textContent = "完了";
+
+    // リストとボタンを追加
+    li.appendChild(trashBtn);
+    list.appendChild(li);
+    addArea.value = "";
+
+    // 完了ボタンで削除
+    trashBtn.addEventListener('click', () => {
+        console.log(list);
+        const target = trashBtn.parentNode;
+        target.remove();
+    });
+});
+
+
+
