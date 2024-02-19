0dd047222efbbbe1aeab76d19b6d9d3a420f33e9
yuunaM <the.megacity@icloud.com>
2024-02-19 15:05:35 +0900

    ヌメロンの修正版

--- numeron/main.js
+++ numeron/main.js
@@ -7,98 +7,104 @@
 // 4. 数字があっていたら3EAT
 
 const check = document.getElementById('numCheck');
-const txt = [
-    document.getElementById('cpNumArea'),
-    document.getElementById('result'),
-    document.getElementById('remainTurn'),
-    document.getElementById('score')
-];
-
+const resultArea = document.getElementById('result');
+const remain = document.getElementById('remainTurn');
+const newGameBtn = document.getElementById('newGame');
+newGameBtn.style.display = "none";
+remain.innerHTML = '残り回数：10回';
+let cpNum = [];
 let turn = 10;
-txt[0].innerHTML = 'CPの数字：';
-txt[1].innerHTML = '結果：';
-txt[2].innerHTML = `残り回数：${turn}回`;
-
-let recordInput = ''; // 入力文字チェック用
-let cpNum = []; // CPの文字列
-let myNum = []; // 自分の回答
-let totalBite = 0; // 累計用
-let totalEat = 0;
-let bite;
-let eat;
-
 
 // 重複のないランダムな3桁の数字を生成
-function opponent() {
+function randomNumber() {
     for (let i = 0; cpNum.length < 3; i++) {
         let randomNum = Math.floor(Math.random() * 10);
         if (!cpNum.includes(randomNum)) {
             cpNum.push(randomNum);
         }
     }
-    txt[0].innerHTML = `CPの数字：${cpNum}`;
+}
+randomNumber();
+
+// ターン毎の履歴を作成
+function TurnHistory(result) {
+    const p = document.createElement('p');
+    p.innerHTML = result;
+    resultArea.appendChild(p);
 }
 
-// eat・biteのチェック
-function judge() {
-    let biteArray = [];
-    bite = 0;
-    eat = 0;
+// 入力値のチェック
+function validation() {
+    const answerNum = document.getElementById('answerNum');
+    if (answerNum.value.length !== 3 || answerNum.value.match(/(.)\1/)) {
+        alert('同じ数字を含まずに3文字で入力してください');
+        return;
+    }
+    TurnHistory(answerNum.value); // 自分の回答を履歴表示用関数に渡す
+    const myNum = answerNum.value.split('').map(Number); // 比較する用に自分の回答を配列化
+    judge(myNum);
+}
+
+// EAT BITEのチェック
+function judge(myNum) {
+    let biteArray = []; // biteチェック用の配列
+    let bite = 0;
+    let eat = 0;
 
     for (let i = 0; i < cpNum.length; i++) {
-        if (cpNum[i] === myNum[i]) { // 同じ数字で同じ桁にある場合は変数eatをカウント
+        if (cpNum[i] === myNum[i]) {
             eat++;
-            totalEat++;
         } 
-        if (cpNum[i] !== myNum[i]) { // eat以外は新たな配列biteArrayに入れてbiteチェックに回す
+        if (cpNum[i] !== myNum[i]) { // 桁と数字が合致しているもののみ取り除いた新たな配列biteArrayを作成
             biteArray.push(myNum[i]);
-        }
-        if (eat === 3) { // eatが3つ揃えば3EAT
-            txt[1].innerHTML = '3EAT!!';
         }
     }
 
-    cpNum.forEach((element) => { // biteArrayとcpNumでbiteをチェック
+    cpNum.forEach((element) => { // biteArrayとcpNumを比較し、biteをチェック
         if (biteArray.includes(element)) {
             bite++;
-            totalBite++;
+        }
+    });
+
+    if ( eat < 3 || bite < 0 ) {
+        TurnHistory(`${bite} BITE ${eat} EAT`);
+    } else {
+        TurnHistory('3EAT');
+        newGame();
+    }
+}
+
+// ターンのカウント
+function checkTurn() {
+    if (turn <= 1) {
+        remain.innerHTML = `終了です！答えは${cpNum}でした`;
+        newGame();
+    } else {
+        turn--;
+        remain.innerHTML = `残り回数：${turn}回`;
+    }
+}
+
+// ニューゲーム
+function newGame() {
+    newGameBtn.style.display = "block";
+    newGameBtn.addEventListener('click', () => {
+        newGameBtn.style.display = "none";
+        cpNum = [];
+        randomNumber();
+        turn = 11;
+        checkTurn();
+        for (let i = resultArea.children.length - 1; i >= 0; i--) {
+            resultArea.removeChild(resultArea.children[i]);
         }
     });
 }
 
-// 挑戦可能回数
-function turnCheck() {
-    if (turn <= 1) {
-        check.disabled = true;
-        txt[2].innerHTML = '終了です！<br> 合計 BITE ' + totalBite + ' EAT ' + totalEat;
-    } else {
-        turn--;
-        txt[2].innerHTML = `残り回数：${turn}回`;
-    }
-}
-
+// 答え合わせをクリックで各関数実行
 check.addEventListener('click', () => {
-    const answerNum = document.getElementById('answerNum').value;
-
-    // 入力文字チェック
-    if (answerNum.length !== 3 || answerNum.match(/(.)\1/)) {
-        alert('同じ数字を含まずに3文字で入力してください');
-        return;
-    }
-    if (recordInput === answerNum) {
-        alert('数字変えない作戦ですか！？');
-        return;
-    } else {
-        recordInput = answerNum;
-    }
-
-    // チェックに通ったら入力値を配列化しCP用の数字を生成してeat・biteチェックを実行
-    myNum = answerNum.split('').map(Number);
-    opponent();
-    judge();
-
-    // ターンごとの結果表示と残り回数の処理
-    txt[1].innerHTML =`結果：${bite} BITE ${eat} EAT`;
-    cpNum = [];
-    turnCheck();
+    validation();
+    checkTurn();
+    answerNum.value = '';
 });
+
+
