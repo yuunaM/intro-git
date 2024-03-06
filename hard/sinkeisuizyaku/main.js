352b8e7e7c6e14f1ff0375ef43eb975855987e83
yuunaM <the.megacity@icloud.com>
2024-03-06 21:08:17 +0900

    神経衰弱発展問題追加

--- hard/sinkeisuizyaku/main.js
+++ hard/sinkeisuizyaku/main.js
@@ -1,15 +1,23 @@
 let count = 0;
+let currentPlayer = 1;
+let player1Point = 0;
+let player2Point = 0;
+const cardNum = [1, 1, 2, 2, 3, 3, 4, 4];
+const nextPlayer = document.getElementById("nextPlayer");
+const player1 = document.getElementById("player1Point");
+const player2 = document.getElementById("player2Point");
+nextPlayer.textContent = `次はplayer${currentPlayer}の番です`;
+player1.textContent = `player1：${player1Point}`;
+player2.textContent = `player2：${player2Point}`;
 
 function shuffle() {
-  const cardNum = [1, 1, 2, 2, 3, 3, 4, 4];
   for (let i = cardNum.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [cardNum[i], cardNum[j]] = [cardNum[j], cardNum[i]];
   }
-  return cardNum;
 }
 
-function createCard(cardNum) {
+function createCard() {
   const panel = document.getElementById("panel");
   let cards = [];
 
@@ -18,8 +26,8 @@ function createCard(cardNum) {
     div.classList.add("card", "back");
 
     div.addEventListener("click", (event) => {
-      checkCardNum(cardNum[i], event, cards);
-    });
+      cardOpen(cardNum[i], event.target, cards);
+    }); // 関数名で判断するのではなく、ここで行うべきかを考える
 
     panel.appendChild(div);
   }
@@ -51,33 +59,61 @@ function restart() {
   });
 }
 
-createCard(shuffle());
-
-function checkCardNum(cardNum, e, cards) {
+function cardOpen(cardNum, target, cards) {
   if (cards.length === 0) {
-    e.target.textContent = cardNum;
-    cards.push(e.target);
-    e.target.classList.remove("back");
-  } else if (cards.length === 1 && e.target.classList.contains("back")) {
-    e.target.textContent = cardNum;
-    cards.push(e.target);
-    e.target.classList.remove("back");
-
-    if (cards[0].textContent === cards[1].textContent) {
-      count++;
-
-      if (count === 4) {
-        setTimeout(() => {
-          restart();
-        }, 550);
-      }
-      setTimeout(() => {
-        finish(cards);
-      }, 500);
-    } else {
-      setTimeout(() => {
-        reverse(cards);
-      }, 500);
-    }
+    target.textContent = cardNum;
+    cards.push(target);
+    target.classList.remove("back");
+  } else if (cards.length === 1 && target.classList.contains("back")) {
+    target.textContent = cardNum;
+    cards.push(target);
+    target.classList.remove("back");
+    checkCardNum(cards);
+    playerChange(cards);
   }
 }
+
+function checkCardNum(cards) {
+  if (cards[0].textContent === cards[1].textContent) {
+    count++;
+    playerPoint(count);
+
+    if (count === 4) {
+      setTimeout(() => {
+        restart();
+      }, 550);
+    }
+    setTimeout(() => {
+      finish(cards);
+    }, 500);
+  } else {
+    setTimeout(() => {
+      reverse(cards);
+    }, 500);
+  }
+}
+
+function playerChange(cards) {
+  if (cards[0].textContent !== cards[1].textContent) {
+    currentPlayer = currentPlayer === 1 ? 2 : 1;
+    nextPlayer.textContent = `次はplayer${currentPlayer}の番です`;
+  }
+}
+
+function playerPoint(count) {
+  console.log(count);
+  if (currentPlayer === 1 && count) {
+    player1Point++;
+    player1.textContent = `player1：${player1Point}`;
+  } else if (currentPlayer === 2 && count) {
+    player2Point++;
+    player2.textContent = `player2：${player2Point}`;
+  }
+}
+
+createCard(shuffle());
+
+// 処理を作ってから関数名を考えてみる。
+// どのタイミングで実行させ対価を考えてみる
+// 再利用性を高めるとどれだけのメリット・デメリットがあるのか考える
+//
